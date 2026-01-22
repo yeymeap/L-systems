b737ebf15eb4f447e51f7b1f292ac63c2383b5c7
Bence Balázs Botló <bence3728@gmail.com>
2026-01-22 14:07:48 +0100

    Move LSystemSettings to separate file
    Extracted the LSystemSettings class from Form1.cs into its own file, LSystemSettings.cs, to improve code organization and maintainability.

--- Form1.cs
+++ Form1.cs
@@ -740,18 +740,4 @@ namespace L_systems
             }
         }
     }
-
-    public class LSystemSettings
-    {
-        public string Axiom { get; set; }
-        public string Rules { get; set; }
-        public string Variables { get; set; }
-        public string Constants { get; set; }
-        public int Iterations { get; set; }
-        public float Angle { get; set; }
-        public float Step { get; set; }
-        public float PenWidth { get; set; }
-        public int CanvasColor { get; set; }
-        public int PenColor { get; set; }
-    }
 }
---
+++ LSystemSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_systems
+{
+    public class LSystemSettings
+    {
+        public string Axiom { get; set; }
+        public string Rules { get; set; }
+        public string Variables { get; set; }
+        public string Constants { get; set; }
+        public int Iterations { get; set; }
+        public float Angle { get; set; }
+        public float Step { get; set; }
+        public float PenWidth { get; set; }
+        public int CanvasColor { get; set; }
+        public int PenColor { get; set; }
+    }
+}

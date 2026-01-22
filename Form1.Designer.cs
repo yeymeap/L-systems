fb8ae1338ffd794d04a727d022b884fdfd5df700
Bence Balázs Botló <bence3728@gmail.com>
2026-01-22 14:27:16 +0100

    Add application icon and resource files
    Set the Form1 icon using a new .ico file and update designer and resource files to support the icon. Added Resources.Designer.cs and Resources.resx for resource management, and updated the project file to include these resources.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             SuspendLayout();
             // 
             // Form1
@@ -35,6 +36,7 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
